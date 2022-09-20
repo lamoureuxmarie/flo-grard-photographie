4d5a0dc31015b68686652ae319636ffd77e94ba0
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-09-20 10:40:48 +0200

    created albums and photos models

---
+++ db/migrate/20220920083925_create_albums.rb
@@ -0,0 +1,11 @@
+class CreateAlbums < ActiveRecord::Migration[7.0]
+  def change
+    create_table :albums do |t|
+      t.string :name
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
